7e12670187729aecc32f681e1387bbf96d1d81d9
Epicsociety <measkswho@gmail.com>
2023-10-08 08:10:59 +0300

    a method that determines if all the boxes can be opened

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/ur/bin/env python3
+"""
+an interview question of dealing with a list of lists
+"""
+
+
+def canUnlockAll(boxes):
+    """
+    check if a key for each box is contained in any of the boxes
+    function takes boxes, a list of lists
+    return: bool
+    """
+
+    keysForBoxes = list(set(boxes[0]) | {0})
+
+    for i in keysForBoxes:
+        for j in boxes[i]:
+            if j not in keysForBoxes:
+                keysForBoxes.append(j)
+            else:
+                continue
+    return len(boxes) == len(keysForBoxes)
