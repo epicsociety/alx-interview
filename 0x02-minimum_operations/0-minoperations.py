1b4905479f8e126a6306d4dacca89083fb42cd06
Epicsociety <measkswho@gmail.com>
2023-10-13 05:52:07 +0300

    interview question, dynamic programming

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -13,15 +13,20 @@ def minOperations(n: int) -> int:
     Return(int): total number of operations
     """
 
-    if n == 1:
+    if n < 2:
         return 0
 
-    dp = [float('inf')] * (n + 1)
-    dp[1] = 0
-
-    for i in range(2, n + 1):
-        for j in range(1, i):
-            if i % j == 0:
-                dp[i] = min(dp[i], dp[j] + i // j)
-
-    return dp[n] if dp[n] != float('inf') else 0
+    operations, characters = 0, 2
+    # a loop that continues as long as charaters are <= n
+    while characters <= n:
+        # checks if n is divisible evenly by characters
+        if n % characters == 0:
+            # If factor of n, adds characters to the operations
+            operations = characters + operations
+            # Update n to be the result of dividing n by characters
+            n = n / characters
+            # Reduces characters by 1 to find other smaller factors
+            characters -= 1
+        # increments characterss by 1 for the next iteration of the loop
+        characters += 1
+    return operations
