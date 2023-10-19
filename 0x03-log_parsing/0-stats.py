1cd1dcd531350ff66196a669d416dff78cc57390
Epicsociety <measkswho@gmail.com>
2023-10-20 00:24:34 +0300

    log parsing

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,37 +1,45 @@
 #!/usr/bin/python3
 """ a script that reads stdin line by line and computes metrics """
 
-def print_stats(status_count, total_size):
-    for code in sorted(status_count.keys()):
-        if status_count[code]:
-            print(f"{code}: {status_count[code]}")
-    print(f"File size: {total_size}")
+import sys
+import re
 
 if __name__ == '__main__':
-    import sys
-    import re
 
     status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
     status_count = {code: 0 for code in status_codes}
     total_size = 0
     count = 0
 
+    def print_stats(status_count, total_size):
+        print(f"File size: {total_size}")
+        for code in sorted(status_count.keys()):
+            if status_count[code]:
+                print(f"{code}: {status_count[code]}")
+
     try:
         for line in sys.stdin:
+            count += 1
             pattern = r'([\d.]+) - \[(.*?)\] "(.*?)" (\d+) (\d+)'
             match = re.match(pattern, line)
 
             if match:
-                ip_address, date, request, status_code, file_size = match.groups()
+                ip_address, date, request, status_code, file_size = match.groups()  # noqa: E501
 
-                if int(status_code) in status_count:
-                    status_count[int(status_code)] += 1
+                try:
+                    if int(status_code) in status_count:
+                        status_count[int(status_code)] += 1
+                except BaseException:
+                    pass
 
-                total_size += int(file_size)
-                count += 1
+                try:
+                    total_size += int(file_size)
+                except BaseException:
+                    pass
 
             if count % 10 == 0:
                 print_stats(status_count, total_size)
+        print_stats(status_count, total_size)
 
     except (KeyboardInterrupt, SystemExit):
         print_stats(status_count, total_size)
