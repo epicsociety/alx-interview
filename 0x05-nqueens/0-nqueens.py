a74ee129de664140ef382f0a57038aac4b477861
Epicsociety <measkswho@gmail.com>
2023-11-05 00:41:40 +0300

    file not run if imported

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -5,50 +5,53 @@ backtracking N queens
 
 import sys
 
-if len(sys.argv) != 2:
-    print('Usage: nqueens N')
-    exit(1)
-
-try:
-    n_q = int(sys.argv[1])
-except ValueError:
-    print('N must be a number')
-    exit(1)
-
-if n_q < 4:
-    print('N must be at least 4')
-    exit(1)
+if __name__=='__main__':
 
 
-def solve_nqueens(n):
-    """Make a choice for the next candidate"""
-    if n == 0:
-        return [[]]
-    inner_solution = solve_nqueens(n - 1)
-    return [solution + [(n, i + 1)]
-            for i in range(n_q)
-            for solution in inner_solution
-            if safe_queen((n, i + 1), solution)]
+    if len(sys.argv) != 2:
+        print('Usage: nqueens N')
+        exit(1)
+
+    try:
+        n_q = int(sys.argv[1])
+    except ValueError:
+        print('N must be a number')
+        exit(1)
+
+    if n_q < 4:
+        print('N must be at least 4')
+        exit(1)
 
 
-def attack_queen(square, queen):
-    """ Undo the choice (backtrack) and try another candidate"""
-    (row1, col1) = square
-    (row2, col2) = queen
-    return (row1 == row2) or (col1 == col2) or\
-        abs(row1 - row2) == abs(col1 - col2)
+    def solve_nqueens(n):
+        """Make a choice for the next candidate"""
+        if n == 0:
+            return [[]]
+        inner_solution = solve_nqueens(n - 1)
+        return [solution + [(n, i + 1)]
+                for i in range(n_q)
+                for solution in inner_solution
+                if safe_queen((n, i + 1), solution)]
 
 
-def safe_queen(sqr, queens):
-    """ Recur with the chosen candidate"""
-    for queen in queens:
-        if attack_queen(sqr, queen):
-            return False
-    return True
+    def attack_queen(square, queen):
+        """ Undo the choice (backtrack) and try another candidate"""
+        (row1, col1) = square
+        (row2, col2) = queen
+        return (row1 == row2) or (col1 == col2) or\
+            abs(row1 - row2) == abs(col1 - col2)
 
 
-for answer in reversed(solve_nqueens(n_q)):
-    result = []
-    for p in [list(p) for p in answer]:
-        result.append([i - 1 for i in p])
-    print(result)
+    def safe_queen(sqr, queens):
+        """ Recur with the chosen candidate"""
+        for queen in queens:
+            if attack_queen(sqr, queen):
+                return False
+        return True
+
+
+    for answer in reversed(solve_nqueens(n_q)):
+        result = []
+        for p in [list(p) for p in answer]:
+            result.append([i - 1 for i in p])
+        print(result)
