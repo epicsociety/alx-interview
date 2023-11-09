f6a674e1d828d44ae18d0fb119b9301cc1b50681
Epicsociety <measkswho@gmail.com>
2023-11-09 12:52:36 +0300

    get characters from starwars API in a synchronous-like flow

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,34 +3,33 @@
 const request = require('request');
 
 const getCharacters = async () => {
-    try {
-        const movieId = process.argv[2];
-        const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
-        
-        const response = await promisifiedRequest(apiUrl);
-        const charsUrl = JSON.parse(response).characters;
+  try {
+    const movieId = process.argv[2];
+    const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
-        for (const url of charsUrl) {
-            const characterResponse = await promisifiedRequest(url);
-            const characterName = JSON.parse(characterResponse).name;
-            console.log(characterName);
-        }
-    } catch (error) {
-        consoler.error(error);
+    const response = await promisifiedRequest(apiUrl);
+    const charsUrl = JSON.parse(response).characters;
+
+    for (const url of charsUrl) {
+      const characterResponse = await promisifiedRequest(url);
+      const characterName = JSON.parse(characterResponse).name;
+      console.log(characterName);
     }
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-
 const promisifiedRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        request.get(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body);
-            }
-        });
+  return new Promise((resolve, reject) => {
+    request.get(url, (error, response, body) => {
+      if (error) {
+        reject(error);
+      } else {
+        resolve(body);
+      }
     });
-}
+  });
+};
 
 getCharacters();
