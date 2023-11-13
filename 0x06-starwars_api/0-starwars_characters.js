468d1d285e9a34d1eb3e67ef5f5e79677e40c348
epicsociety <measkswho@gmail.com>
2023-11-13 05:28:37 -0800

    Star wars API

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,14 +3,6 @@ const util = require('util');
 const request = util.promisify(require('request'));
 const filmID = process.argv[2];
 
-<<<<<<< HEAD
-#!/usr/bin/node
-const util = require('util');
-const request = util.promisify(require('request'));
-const filmID = process.argv[2];
-
-=======
->>>>>>> 50c4cecfd9304a48c74bd68e9f5bec01316ec350
 async function starwarsCharacters (filmId) {
   const endpoint = 'https://swapi-api.hbtn.io/api/films/' + filmId;
   let response = await (await request(endpoint)).body;
@@ -26,46 +18,3 @@ async function starwarsCharacters (filmId) {
 }
 
 starwarsCharacters(filmID);
-<<<<<<< HEAD
-=======
-
-// #!/usr/bin/node
-
-// const request = require('request');
-// const movieId = process.argv[2];
-// const url = `https://swapi-api.alx-tools.com/api/films/${movieId}/`;
-
-// request.get(url, function (error, response, body) {
-//   if (error) {
-//     console.error(error);
-//     return;
-//   }
-//   const charactersUrls = JSON.parse(body).characters;
-//   const fetchCharacterPromises = [];
-
-//   charactersUrls.forEach((character) => {
-//     const fetchPromise = new Promise((resolve, reject) => {
-//       request.get(character, function (error, response, body) {
-//         if (error) {
-//           console.error(error);
-//           return;
-//         }
-
-//         const characterName = JSON.parse(body).name;
-//         resolve(characterName);
-//       });
-//     });
-//     fetchCharacterPromises.push(fetchPromise);
-//   });
-
-//   Promise.all(fetchCharacterPromises)
-//     .then((characterNames) => {
-//       characterNames.forEach((name) => {
-//         console.log(name);
-//       });
-//     })
-//     .catch((error) => {
-//       console.erro(error);
-//     });
-// });
->>>>>>> 50c4cecfd9304a48c74bd68e9f5bec01316ec350
