52300bc2e8ea3b3c8d227ca7da791cc0ee45792a
Epicsociety <measkswho@gmail.com>
2023-11-28 09:12:53 +0300

    calculate the perimeter of an island presented by the grid

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -23,15 +23,16 @@ def island_perimeter(grid: List[List[int]]) -> int:
 #                     perimeter += 1
 
 #     return perimeter
+    if rows <= 100 and columns <= 100:
+        perimeter = 0
+        for i in range(rows):
+            for j in range(columns):
+                if grid[i][j] == 1:
+                    perimeter += 4
+                    if i > 0 and grid[i - 1][j] == 1:
+                        perimeter -= 2
+                    if j > 0 and grid[i][j - 1] == 1:
+                        perimeter -= 2
 
-    perimeter = 0
-    for i in range(rows):
-        for j in range(columns):
-            if grid[i][j] == 1:
-                perimeter += 4
-                if i > 0 and grid[i - 1][j] == 1:
-                    perimeter -= 2
-                if j > 0 and grid[i][j - 1] == 1:
-                    perimeter -= 2
-
-    return perimeter
+        return perimeter
+    return
